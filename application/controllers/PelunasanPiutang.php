16ad604dd277820789580f315ec7f2f3e39cfcc2
kevinyahya <okevin0107@gmail.com>
2017-12-14 13:23:42 +0700

    piutang
    asd

--- application/controllers/PelunasanPiutang.php
+++ application/controllers/PelunasanPiutang.php
@@ -25,6 +25,8 @@ class PelunasanPiutang extends CI_Controller {
         $this->load->model('Bank_model');
         $this->load->model('NotaJual_model');
         $this->load->model('Pembelian_model');
+        $this->load->model('Jurnal_model');
+        $this->load->model('JurnalHasAkun_model');
         $this->load->model('PelunasanPiutang_model');
         $this->load->helper('url_helper');
         $this->load->helper('form');
@@ -57,21 +59,112 @@ class PelunasanPiutang extends CI_Controller {
 	public function simpan(){
 		$nota = $this->input->post('noNota');
 		$tgl = $this->input->post('tgl');
-		$jp = $this->input->post('jPembayaran');
 		$nominal = $this->input->post('nominal');
 		$disc = $this->input->post('discPelunasan');
 		$bayar = $this->input->post('totalBayar');
+		$jp = $this->input->post('jPembayaran');
+		$nocek = $this->input->post('noCek');
+
+		$temp = $this->PelunasanPiutang_model->get_last_pelunasan();
+		$idPelunasan = $temp['NoPelunasan']+1; 	
+
+		$temp2 = $this->Jurnal_model->get_last_jurnal();
+		$IDJurnal = $temp2['IDJurnal']+1;
 
 		$dataSimpan = array(
+			'NoPelunasan' => $idPelunasan,
 			'Tanggal' => $tgl,
 			'NominalSeharusnya' => $nominal,
 			'DiskonPelunasan' => $disc,
 			'Bayar' => $bayar,
 			'JenisPembayaran' => $jp,
-			'NoNotaJual' => $nota
+			'NoNotaJual' => $nota,
+			'NoCek' => $nocek,
 		);
 
 		if($this->PelunasanPiutang_model->add_pelunasan_piutang($dataSimpan)){
+			$notajual = $this->NotaJual_model->get_nota($nota);
+			$keterangan ='Pelunasan transaksi penjualan tanggal '.$notajual['Tanggal'];
+			$akun ="";
+			if($jp=="T"){
+				$keterangan.=' Secara tunai';
+				$akun="101";
+			}
+			else if($jp=="TR"){
+				$keterangan.=' dengan transfer bank';
+				if($idbank=="1")
+					$akun="102";
+				else
+					$akun="103";	
+			}
+			else{
+				$keterangan.=' dengan cek';		
+				$akun="203";
+			}
+
+
+			$dataJurnal = array(
+				'IDJurnal' =>$IDJurnal,
+				'Tanggal' => date('Y-m-d'),
+				'Keterangan'=> $keterangan,
+				'NoBukti'=> $idPelunasan,
+				'JenisJurnal' => 'JU',
+				'IDPeriode' => '20172');
+
+			//insert jurnal 
+			if($this->Jurnal_model->add_jurnal($dataJurnal)){
+				if($disc>0){
+					$totalDiskon=($nominal*$disc)/100;
+					$tempNominal = $nominal-$totalDiskon;
+					//insert akun pembayaran entah itu kas,rekening, atau cek
+					$data = array(
+						'IDJurnal' =>$IDJurnal,
+						'NoAkun' => $akun,
+						'Urutan' =>1,
+						'NominalDebet' => $tempNominal,
+						'NominalKredit' =>0, 
+					);
+					$this->JurnalHasAkun_model->add_jurnalHasAkun($data);
+
+					$data = array(
+					'IDJurnal' =>$IDJurnal,
+					'NoAkun' => '104',
+					'Urutan' =>2,
+					'NominalDebet' => 0,
+					'NominalKredit' =>$nominal, 
+					);
+					$this->JurnalHasAkun_model->add_jurnalHasAkun($data);
+
+					$data = array(
+					'IDJurnal' =>$IDJurnal,
+					'NoAkun' => '402',
+					'Urutan' =>3,
+					'NominalDebet' => $totalDiskon,
+					'NominalKredit' => 0, 
+					);
+					$this->JurnalHasAkun_model->add_jurnalHasAkun($data);
+				}//Jika Tidak ada diskon
+				else{
+					//insert akun pembayaran entah itu kas,rekening, atau cek
+					$data = array(
+						'IDJurnal' =>$IDJurnal,
+						'NoAkun' => $akun,
+						'Urutan' =>1,
+						'NominalDebet' => $nominal,
+						'NominalKredit' => 0, 
+					);
+					$this->JurnalHasAkun_model->add_jurnalHasAkun($data);
+
+					$data = array(
+					'IDJurnal' =>$IDJurnal,
+					'NoAkun' => '104',
+					'Urutan' =>2,
+					'NominalDebet' => 0,
+					'NominalKredit' =>$nominal, 
+					);
+					$this->JurnalHasAkun_model->add_jurnalHasAkun($data);
+				}
+			}
 			header("Location: ".site_url('PelunasanPiutang/index'));
 		}
 	}
